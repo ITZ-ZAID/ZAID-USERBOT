a38fc1e987cb089e169ebfa407a22d64a4949150
Zaid <95952679+ITZ-ZAID@users.noreply.github.com>
2022-05-15 22:16:22 +0530

    Create Unload.py

---
+++ handlers/Unload.py
@@ -0,0 +1,41 @@
+from pyrogram import Client, filters
+from pyrogram.types import *
+from handlers.restarter import restart
+import os
+module_list = {}
+
+
+
+
+@Client.on_message(filters.command('unloadmod', prefixes=prefix) & filters.me)
+async def unloadmod(client: Client, message: Message):
+    try:
+        module_name = message.text.replace(f'{prefix}unloadmod', '')
+        params = module_name.split()
+        module_name = params[0]
+        del module_list[module_name]
+        file = file_list[module_name]
+        os.remove(f'handlers/{file}')
+        await message.edit("**The module has been successfully unloaded.**\nRestart...")
+        await restart(message, restart_type="restart")
+    except Exception as error:
+        await message.edit(f"**An error has occurred.**\nLog: not found {error}")
+
+
+
+@Client.on_message(filters.command(['loadmod', "install"], prefixes=prefix) & filters.me)
+async def loadmod(client, message):
+    if not message.reply_to_message:
+        await message.edit("<b>Load module...</b>")
+        link = message.command[1]
+        wget.download(link, 'handlers/')
+        await message.edit(
+            f"<b>**The module has been loaded successfully.**\nRestart..."
+        )
+        await restart(message, restart_type="restart")
+    else:
+        await client.download_media(message.reply_to_message.document, file_name='plugins/')
+        await message.edit(
+            f"<b>**The module has been loaded successfully.**\nRestart..."
+        )
+        await restart(message, restart_type="restart")
