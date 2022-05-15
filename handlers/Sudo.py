3ad29c01cc0749594d6e746794590082e14a2b75
Zaid <95952679+ITZ-ZAID@users.noreply.github.com>
2022-05-15 15:41:15 +0530

    Create Sudo.py

---
+++ handlers/Sudo.py
@@ -0,0 +1,121 @@
+# Own
+
+from pyrogram.types import Message
+import asyncio
+from pyrogram import filters, Client
+from config import SUDO_USERS
+
+@Client.on_message(filters.user(SUDO_USERS) & filters.command(["delspam", "deletespam"], [".", "!", "/"]))
+async def statspam(client: Client, message: Message):
+    zaid = await message.reply_text("⚡ Usage:\n /delspam 10 Umm")
+    quantity = message.command[1]
+    spam_text = ' '.join(message.command[2:])
+    quantity = int(quantity)
+    await message.delete()
+    for i in range(quantity):
+        await zaid.delete()
+        msg = await client.send_message(message.chat.id, spam_text)
+        await asyncio.sleep(0.1)
+        await msg.delete()
+        await asyncio.sleep(0.1)
+
+
+@Client.on_message(filters.user(SUDO_USERS) & filters.command(["spam", "spamming"], [".", "!", "/"]))
+async def sspam(client: Client, message: Message):
+    zaid = await message.reply_text("⚡ Usage:\n /spam 10 Umm")
+    quantity = message.command[1]
+    spam_text = ' '.join(message.command[2:])
+    quantity = int(quantity)
+
+    if message.reply_to_message:
+        reply_to_id = message.reply_to_message.message_id
+        for _ in range(quantity):
+            await client.send_message(message.chat.id, spam_text,
+                                      reply_to_message_id=reply_to_id)
+            await asyncio.sleep(0.15)
+        return
+
+    for _ in range(quantity):
+        await zaid.delete()
+        await client.send_message(message.chat.id, spam_text)
+        await asyncio.sleep(0.15)
+
+
+@Client.on_message(filters.user(SUDO_USERS) & filters.command(["fastspam"], [".", "!", "/"]))
+async def fastspam(client: Client, message: Message):
+    zaid = await message.reply_text("⚡ Usage:\n /fastspam 10 Umm")
+    quantity = message.command[1]
+    spam_text = ' '.join(message.command[2:])
+    quantity = int(quantity)
+    
+    if message.reply_to_message:
+        reply_to_id = message.reply_to_message.message_id
+        for _ in range(quantity):
+            await client.send_message(message.chat.id, spam_text,
+                                      reply_to_message_id=reply_to_id)
+            await asyncio.sleep(0.002)
+        return
+    
+    for _ in range(quantity):
+        await zaid.delete()
+        await client.send_message(message.chat.id, spam_text)
+        await asyncio.sleep(0.002)
+
+
+
+@Client.on_message(filters.user(SUDO_USERS) & filters.command(["slowspam", "delayspam"], [".", "!", "/"]))
+async def slowspam(client: Client, message: Message):
+    zaid = await message.reply_text("⚡ Usage:\n /slowspam 10 Umm")
+    quantity = message.command[1]
+    spam_text = ' '.join(message.command[2:])
+    quantity = int(quantity)
+
+    if message.reply_to_message:
+        reply_to_id = message.reply_to_message.message_id
+        for _ in range(quantity):
+            await client.send_message(message.chat.id, spam_text,
+                                      reply_to_message_id=reply_to_id)
+            await asyncio.sleep(0.9)
+        return
+
+    for _ in range(quantity):
+        await zaid.delete()
+        msg = await client.send_message(message.chat.id, spam_text)
+        await asyncio.sleep(0.9)
+
+
+
+
+
+
+
+@Client.on_message(filters.user(SUDO_USERS) & filters.command(["sspam", "stkspam", "spamstk", "stickerspam"], [".", "!", "/"]))
+async def spam_stick(client: Client, message: Message):
+    if not message.reply_to_message:
+        await message.edit_text("**reply to a sticker with amount you want to spam**")
+        return
+    if not message.reply_to_message.sticker:
+        await message.edit_text(text="**reply to a sticker with amount you want to spam**")
+        return
+    else:
+        i=0
+        times = message.command[1]
+        if message.chat.type in ["supergroup", "group"]:
+            for i in range(int(times)):
+                sticker=message.reply_to_message.sticker.file_id
+                await client.send_sticker(
+                    message.chat.id,
+                    sticker,
+                )
+                await asyncio.sleep(0.10)
+
+        if umm.chat.type == "private":
+            for i in range(int(times)):
+                sticker=message.reply_to_message.sticker.file_id
+                await client.send_sticker(
+                    message.chat.id, sticker
+                )
+                await asyncio.sleep(0.10)
+
+
+
