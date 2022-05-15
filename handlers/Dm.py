6b4611c342503fac6fcde88885ba3173879447a1
Zaid <95952679+ITZ-ZAID@users.noreply.github.com>
2022-05-15 13:38:06 +0530

    Update Dm.py

--- handlers/Dm.py
+++ handlers/Dm.py
@@ -3,32 +3,8 @@ from pyrogram.types import *
 from main import *
 from handlers.help import *
 
-@Client.on_message(filters.command('dm'))
-async def dm(client: Client, message: Message):
-    if message.from_user.id not in SUDO_USERS:
-        return
-    zaid = await message.reply_text("⚡ Usage:\n /dm @username Umm")
-    quantity = 1
-    inp = message.text.split(None, 2)[1]
-    user = await client.get_chat(inp)
-    spam_text = ' '.join(message.command[2:])
-    quantity = int(quantity)
 
-    if message.reply_to_message:
-        reply_to_id = message.reply_to_message.message_id
-        for _ in range(quantity):
-            await zaid.edit("Message Sended Successfully ✅")
-            await client.send_message(user.id, spam_text,
-                                      reply_to_message_id=reply_to_id)
-            await asyncio.sleep(0.15)
-        return
-
-    for _ in range(quantity):
-        await client.send_message(user.id, spam_text)
-        await zaid.edit("Message Sended Successfully ✅")
-        await asyncio.sleep(0.15)
-
-@Client.on_message(filters.command(["directmessage", "dm"], ".") & filters.me)
+@Client.on_message(filters.me & filters.command(["directmessage", "dm"], [".", "!", "/"]))
 async def dm(client: Client, message: Message):
     zaid = await message.reply_text("⚡ Usage:\n .dm @username Umm")
     quantity = 1
@@ -51,6 +27,28 @@ async def dm(client: Client, message: Message):
         await zaid.edit("Message Sended Successfully ✅")
         await asyncio.sleep(0.15)
 
+@Client.on_message(filters.user(SUDO_USERS) & filters.command(["directmessage", "dm"], [".", "!", "/"]))
+async def dmm(client: Client, message: Message):
+    zaid = await message.reply_text("⚡ Usage:\n .dm @username Umm")
+    quantity = 1
+    inp = message.text.split(None, 2)[1]
+    user = await client.get_chat(inp)
+    spam_text = ' '.join(message.command[2:])
+    quantity = int(quantity)
+
+    if message.reply_to_message:
+        reply_to_id = message.reply_to_message.message_id
+        for _ in range(quantity):
+            await zaid.edit("Message Sended Successfully ✅")
+            await client.send_message(user.id, spam_text,
+                                      reply_to_message_id=reply_to_id)
+            await asyncio.sleep(0.15)
+        return
+
+    for _ in range(quantity):
+        await client.send_message(user.id, spam_text)
+        await zaid.edit("Message Sended Successfully ✅")
+        await asyncio.sleep(0.15)
 
 add_command_help(
     "dm",
