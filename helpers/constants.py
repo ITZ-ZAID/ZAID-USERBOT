c751bf4c099f5aef05312a6a43241de310a8bfdc
Zaid <95952679+ITZ-ZAID@users.noreply.github.com>
2022-05-15 09:52:43 +0530

    K

--- helpers/constants.py
+++ helpers/constants.py
@@ -1,7 +1,7 @@
 class First:
     ALIVE = "`Still Working On Fire 🔥`"
-    REPO = 'Click <a href="GitHub.com/Itz-Zaid/Multi-Userbot">here</a> to open Usebot\'s GitHub page.'
-    CREATOR = 'I was created by my master <a href="https://github.com/ITZ-ZAID">ZId</a> on a rainy day.'
+    REPO = 'Click <a href="GitHub.com/Itz-Zaid/Zaid-Userbot">here</a> to open Usebot\'s GitHub page.'
+    CREATOR = 'I was created by my master <a href="https://github.com/ITZ-ZAID">Zaid</a> on a rainy day.'
 
 
 class Weebify:
