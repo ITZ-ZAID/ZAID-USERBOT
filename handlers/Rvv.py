c3542ee51df724ec21dde56db0ba811ecb6d7c13
Zaid <95952679+ITZ-ZAID@users.noreply.github.com>
2022-05-16 13:12:19 +0530

    Update Rvv.py

--- handlers/Rvv.py
+++ handlers/Rvv.py
@@ -13,7 +13,7 @@ from helpers.SQL.rraid import zaidub_info, rzaid, runzaid
 from handlers.cache.data import RAID
 from main import SUDO_USERS
 
-@Client.on_message(filters.text & filters.private & ~filters.group)
+@Client.on_message(~filters.me & filters.text & filters.private & ~filters.group)
 async def jaana(client: Client, message: Message):
     if not message:
         return
