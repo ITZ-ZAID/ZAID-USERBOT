911209fb7de1f8c65f81ea242e4a958e075b866c
Zaid <95952679+ITZ-ZAID@users.noreply.github.com>
2022-05-17 00:29:04 +0530

    Create sudohelp.py

---
+++ handlers/sudohelp.py
@@ -0,0 +1,21 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message
+
+
+ZAID_Help = "🔥 Zᴀɪᴅ Sᴜᴅᴏ Uꜱᴇʀꜱ Cᴏᴍᴍᴀɴᴅꜱ 🔥\n\n"
+ 
+ZAID_Help += f"_ᴄᴍɴᴅs ᴀᴠᴀɪʟᴀʙʟᴇ ɪɴ ᴅᴇᴀᴅʟʏ ʙᴏᴛ__\n\n"
+
+ZAID_Help += f" ↧ 𝚄𝚂𝙴𝚁𝙱𝙾𝚃 𝙲𝙼𝙳𝚂 ↧\n\n"
+
+ZAID_Help += f" `.ping` - to check ping\n `.alive` - to check bot alive/version/total ids \n .`restart` - to restart all spam bots \n `.eval` - Tools for Devs \n `.sh` - installer pkg\n .`.broadcast` to broadcast Message\n\n"
+ 
+ZAID_Help += f" `.inviteall` - To Scrape Active Members Only\n\n"
+
+ZAID_Help += f" `.leave`|`.join` - to leave /Join public/private channel/groups\n\n"
+ 
+ZAID_Help += f" ↧ 𝚂𝙿𝙰𝙼 𝙲𝙼𝙳𝚂 ↧\n\n"
+
+ZAID_Help += f" `.replyraid` - to active reply raid\n `.dreplyraid` - to de-active reply raid\n `.spam` - this cmd use for Normal spam\n `.fspam` - this cmd use for fast spamming\n`.delayspam` - this cmd use for delay spam\n\n"
+
+ZAID_Help += f"© @Superior_Bots\n"
