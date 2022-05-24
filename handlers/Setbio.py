28816b87a5872e9963a5da6e81b3ff14c94d71e0
Zaid <95952679+ITZ-ZAID@users.noreply.github.com>
2022-05-24 21:12:47 +0530

    Update Setbio.py

--- handlers/Setbio.py
+++ handlers/Setbio.py
@@ -1,6 +1,7 @@
 from pyrogram import filters, Client
 from pyrogram.types import *
 from inspect import getfullargspec
+from config import SUDO_USERS
 
 from pyrogram import Client, filters
 from pyrogram.raw.functions.messages import DeleteHistory
@@ -13,97 +14,6 @@ from pyrogram.types import (CallbackQuery, InlineKeyboardButton,
 flood = {}
 
 
-@Client.on_message(
-    filters.private
-    & filters.incoming
-    & ~filters.service
-    & ~filters.edited
-    & ~filters.me
-    & ~filters.bot
-    & ~filters.via_bot
-    & ~filters.user(SUDOERS)
-)
-async def awaiting_message(client, message):
-    if await is_on_off(5):
-        try:
-            await client.forward_messages(
-                chat_id=LOG_GROUP_ID,
-                from_chat_id=message.from_user.id,
-                message_ids=message.message_id,
-            )
-        except Exception as err:
-            pass
-    user_id = message.from_user.id
-    if await is_pmpermit_approved(user_id):
-        return
-    async for m in client.iter_history(user_id, limit=6):
-        if m.reply_markup:
-            await m.delete()
-    if str(user_id) in flood:
-        flood[str(user_id)] += 1
-    else:
-        flood[str(user_id)] = 1
-    if flood[str(user_id)] > 5:
-        await message.reply_text("Spam Detected. User Blocked")
-        await client.send_message(
-            LOG_GROUP_ID,
-            f"**Spam Detect Block On Assistant**\n\n- **Blocked User:** {message.from_user.mention}\n- **User ID:** {message.from_user.id}",
-        )
-        return await client.block_user(user_id)
-    await message.reply_text(
-        f"Hello, I am {MUSIC_BOT_NAME}'s Assistant.\n\nPlease dont spam here , else you'll get blocked.\nFor more Help start :- @{BOT_USERNAME}"
-    )
-
-
-@Client.on_message(
-    filters.command("approve", prefixes=ASSISTANT_PREFIX)
-    & filters.user(SUDOERS)
-    & ~filters.via_bot
-)
-@Client.on_message(
-    filters.command("approve", prefixes=ASSISTANT_PREFIX)
-    & filters.user("me")
-    & ~filters.via_bot
-)
-async def pm_approve(client, message):
-    if not message.reply_to_message:
-        return await eor(
-            message, text="Reply to a user's message to approve."
-        )
-    user_id = message.reply_to_message.from_user.id
-    if await is_pmpermit_approved(user_id):
-        return await eor(message, text="User is already approved to pm")
-    await approve_pmpermit(user_id)
-    await eor(message, text="User is approved to pm")
-
-
-@Client.on_message(
-    filters.command("disapprove", prefixes=ASSISTANT_PREFIX)
-    & filters.user(SUDOERS)
-    & ~filters.via_bot
-)
-@Client.on_message(
-    filters.command("disapprove", prefixes=ASSISTANT_PREFIX)
-    & filters.user("me")
-    & ~filters.via_bot
-)
-async def pm_disapprove(client, message):
-    if not message.reply_to_message:
-        return await eor(
-            message, text="Reply to a user's message to disapprove."
-        )
-    user_id = message.reply_to_message.from_user.id
-    if not await is_pmpermit_approved(user_id):
-        await eor(message, text="User is already disapproved to pm")
-        async for m in client.iter_history(user_id, limit=6):
-            if m.reply_markup:
-                try:
-                    await m.delete()
-                except Exception:
-                    pass
-        return
-    await disapprove_pmpermit(user_id)
-    await eor(message, text="User is disapproved to pm")
 
 
 @Client.on_message(
