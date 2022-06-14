7e7000f645941f798c890f01d8df5729115ed2e0
Azra8800 <94593735+Azra8800@users.noreply.github.com>
2022-06-14 13:26:24 +0530

    Update spam.py

--- handlers/spam.py
+++ handlers/spam.py
@@ -2,131 +2,90 @@
 
 from pyrogram.types import Message
 import asyncio
-import asyncio
-from pyrogram import filters, Client
-from handlers.help import *
+from asyncio import wait, sleep
 
-@Client.on_message(filters.me & filters.command(["delspam", "deletespam"], [".", "!", "/"]))
-async def statspam(client: Client, message: Message):
-    zaid = await message.reply_text("⚡ Usage:\n /delspam 10 Umm")
-    quantity = message.command[1]
-    spam_text = ' '.join(message.command[2:])
-    quantity = int(quantity)
-    await message.delete()
-    for i in range(quantity):
-        await zaid.delete()
-        msg = await client.send_message(message.chat.id, spam_text)
-        await asyncio.sleep(0.1)
-        await msg.delete()
-        await asyncio.sleep(0.1)
+from userbot import BOTLOG, BOTLOG_CHATID, CMD_HELP
+from userbot.events import register
 
 
-@Client.on_message(filters.me & filters.command(["spam", "spamming"], [".", "!", "/"]))
-async def sspam(client: Client, message: Message):
-    zaid = await message.reply_text("⚡ Usage:\n /spam 10 Umm")
-    quantity = message.command[1]
-    spam_text = ' '.join(message.command[2:])
-    quantity = int(quantity)
-
-    if message.reply_to_message:
-        reply_to_id = message.reply_to_message.message_id
-        for _ in range(quantity):
-            await client.send_message(message.chat.id, spam_text,
-                                      reply_to_message_id=reply_to_id)
-            await asyncio.sleep(0.15)
-        return
-
-    for _ in range(quantity):
-        await zaid.delete()
-        await client.send_message(message.chat.id, spam_text)
-        await asyncio.sleep(0.15)
+@register(outgoing=True, pattern="^\.cspam (.*)")
+async def tmeme(e):
+    cspam = str(e.pattern_match.group(1))
+    message = cspam.replace(" ", "")
+    await e.delete()
+    for letter in message:
+        await e.respond(letter)
+    if BOTLOG:
+        await e.client.send_message(
+            BOTLOG_CHATID, "#CSPAM\n"
+            "TSpam was executed successfully")
 
 
-@Client.on_message(filters.me & filters.command(["fastspam"], [".", "!", "/"]))
-async def fastspam(client: Client, message: Message):
-    zaid = await message.reply_text("⚡ Usage:\n /fastspam 10 Umm")
-    quantity = message.command[1]
-    spam_text = ' '.join(message.command[2:])
-    quantity = int(quantity)
-    
-    if message.reply_to_message:
-        reply_to_id = message.reply_to_message.message_id
-        for _ in range(quantity):
-            await client.send_message(message.chat.id, spam_text,
-                                      reply_to_message_id=reply_to_id)
-            await asyncio.sleep(0.002)
-        return
-    
-    for _ in range(quantity):
-        await zaid.delete()
-        await client.send_message(message.chat.id, spam_text)
-        await asyncio.sleep(0.002)
+@register(outgoing=True, pattern="^\.wspam (.*)")
+async def tmeme(e):
+    wspam = str(e.pattern_match.group(1))
+    message = wspam.split()
+    await e.delete()
+    for word in message:
+        await e.respond(word)
+    if BOTLOG:
+        await e.client.send_message(
+            BOTLOG_CHATID, "#WSPAM\n"
+            "WSpam was executed successfully")
 
 
-
-@Client.on_message(filters.me & filters.command(["slowspam", "delayspam"], [".", "!", "/"]))
-async def slowspam(client: Client, message: Message):
-    zaid = await message.reply_text("⚡ Usage:\n /slowspam 10 Umm")
-    quantity = message.command[1]
-    spam_text = ' '.join(message.command[2:])
-    quantity = int(quantity)
-
-    if message.reply_to_message:
-        reply_to_id = message.reply_to_message.message_id
-        for _ in range(quantity):
-            await client.send_message(message.chat.id, spam_text,
-                                      reply_to_message_id=reply_to_id)
-            await asyncio.sleep(0.9)
-        return
-
-    for _ in range(quantity):
-        await zaid.delete()
-        msg = await client.send_message(message.chat.id, spam_text)
-        await asyncio.sleep(0.9)
+@register(outgoing=True, pattern="^\.spam (.*)")
+async def spammer(e):
+    counter = int(e.pattern_match.group(1).split(' ', 1)[0])
+    spam_message = str(e.pattern_match.group(1).split(' ', 1)[1])
+    await e.delete()
+    await asyncio.wait([e.respond(spam_message) for i in range(counter)])
+    if BOTLOG:
+        await e.client.send_message(BOTLOG_CHATID, "#SPAM\n"
+                                    "Spam was executed successfully")
 
 
+@register(outgoing=True, pattern="^\.picspam")
+async def tiny_pic_spam(e):
+    message = e.text
+    text = message.split()
+    counter = int(text[1])
+    link = str(text[2])
+    await e.delete()
+    for i in range(1, counter):
+        await e.client.send_file(e.chat_id, link)
+    if BOTLOG:
+        await e.client.send_message(
+            BOTLOG_CHATID, "#PICSPAM\n"
+            "PicSpam was executed successfully")
 
 
+@register(outgoing=True, pattern="^\.delayspam (.*)")
+async def spammer(e):
+    spamDelay = float(e.pattern_match.group(1).split(' ', 2)[0])
+    counter = int(e.pattern_match.group(1).split(' ', 2)[1])
+    spam_message = str(e.pattern_match.group(1).split(' ', 2)[2])
+    await e.delete()
+    for i in range(1, counter):
+        await e.respond(spam_message)
+        await sleep(spamDelay)
+    if BOTLOG:
+        await e.client.send_message(
+            BOTLOG_CHATID, "#DelaySPAM\n"
+            "DelaySpam was executed successfully")
 
 
-
-@Client.on_message(filters.me & filters.command(["sspam", "stkspam", "spamstk", "stickerspam"], [".", "!", "/"]))
-async def spam_stick(client: Client, message: Message):
-    if not message.reply_to_message:
-        await message.edit_text("**reply to a sticker with amount you want to spam**")
-        return
-    if not message.reply_to_message.sticker:
-        await message.edit_text(text="**reply to a sticker with amount you want to spam**")
-        return
-    else:
-        i=0
-        times = message.command[1]
-        if message.chat.type in ["supergroup", "group"]:
-            for i in range(int(times)):
-                sticker=message.reply_to_message.sticker.file_id
-                await client.send_sticker(
-                    message.chat.id,
-                    sticker,
-                )
-                await asyncio.sleep(0.10)
-
-        if umm.chat.type == "private":
-            for i in range(int(times)):
-                sticker=message.reply_to_message.sticker.file_id
-                await client.send_sticker(
-                    message.chat.id, sticker
-                )
-                await asyncio.sleep(0.10)
-
-
-
-add_command_help(
-    "dm",
-    [
-        [".delspam", "It will Spam then delete it's spam automatically."],
-        [".spam", "Spam Your Custom Message  (Sudo User also)."],
-        [".sspam", "Sticker Spam  (Sudo Users also)."],
-        [".delayspam", "Spam Slowly (Sudo User also)."],
-        [".fastspam", "Spam Your message fastly  (Sudo User also)."],
-    ],
-)
+CMD_HELP.update({
+    "spam":
+    ".cspam <text>\
+\nUsage: Spam the text letter by letter.\
+\n\n.spam <count> <text>\
+\nUsage: Floods text in the chat !!\
+\n\n.wspam <text>\
+\nUsage: Spam the text word by word.\
+\n\n.picspam <count> <link to image/gif>\
+\nUsage: As if text spam was not enough !!\
+\n\n.delayspam <delay> <count> <text>\
+\nUsage: .bigspam but with custom delay.\
+\n\n\nNOTE : Spam at your own risk !!"
+})
