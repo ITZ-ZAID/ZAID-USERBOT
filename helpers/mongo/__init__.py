a3cdf79feb74009e18ae0f626d6c14448527f96f
Zaid <itsunknown122@gmail.com>
2022-06-30 10:07:13 +0530

    Added asyncio

--- helpers/mongo/__init__.py
+++ helpers/mongo/__init__.py
@@ -1,5 +1,5 @@
 import motor.motor_asyncio
-from main import MONGO_DB_URI
+from main import MONGO_DB
 
 
-cli = motor.motor_asyncio.AsyncIOMotorClient(MONGO_DB_URI)
+cli = motor.motor_asyncio.AsyncIOMotorClient(MONGO_DB)
