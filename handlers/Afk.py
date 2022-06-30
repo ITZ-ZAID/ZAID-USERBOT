8fa35f374f11dad6e9c471e33836dcdf6231ca2c
Zaid <itzzaid@pm.me>
2022-06-30 10:33:27 +0530

    Update Afk.py

--- handlers/Afk.py
+++ handlers/Afk.py
@@ -23,10 +23,37 @@ from config import PREFIX, LOG_CHAT
 from helpers.pyrohelper import get_arg
 import helpers.mongo.afkdb as Zaid
 from helpers.pyrohelper import user_afk
-from handlers.alive import get_readable_time
 from helpers.utils import get_message_type, Types
 
 LOG_CHAT = LOG_GROUP
+import asyncio
+import time
+from datetime import datetime
+
+def get_readable_time(seconds: int) -> str:
+    count = 0
+    ping_time = ""
+    time_list = []
+    time_suffix_list = ["s", "m", "h", "days"]
+
+    while count < 4:
+        count += 1
+        remainder, result = divmod(seconds, 60) if count < 3 else divmod(seconds, 24)
+        if seconds == 0 and remainder == 0:
+            break
+        time_list.append(int(result))
+        seconds = int(remainder)
+
+    for x in range(len(time_list)):
+        time_list[x] = str(time_list[x]) + time_suffix_list[x]
+    if len(time_list) == 4:
+        ping_time += time_list.pop() + ", "
+
+    time_list.reverse()
+    ping_time += ":".join(time_list)
+
+    return ping_time
+
 
 MENTIONED = []
 AFK_RESTIRECT = {}
