3bedf41df26e4ea0a3bca30a8ff12b0cb703317c
Zaid <itzzaid@pm.me>
2022-07-01 22:59:28 +0530

    Update global.py

--- handlers/global.py
+++ handlers/global.py
@@ -6,21 +6,6 @@ from helpers.pyrohelper import get_arg
 from helpers.adminhelpers import CheckAdmin
 
 
-@Client.on_message(filters.command("gmute", ["."]) & filters.me)
-async def gmute(app: Client, message):
-    reply = message.reply_to_message
-    if reply:
-        user = reply.from_user["id"]
-    else:
-        user = get_arg(message)
-        if not user:
-            await message.edit("**Whome should I gmute?**")
-            return
-    get_user = await app.get_users(user)
-    await gmute_user(get_user.id)
-    await message.edit(f"**Successfully Taped {get_user.first_name}, This users mouth!**")
-
-
 @Client.on_message(filters.command("ungmute", ["."]) & filters.me)
 async def gmute(app: Client, message):
     reply = message.reply_to_message
