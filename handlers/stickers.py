a7297d602ebe46f42912d785d11825adb770bd5c
Zaid <itzzaid@pm.me>
2022-07-02 19:27:36 +0530

    Update stickers.py

--- handlers/stickers.py
+++ handlers/stickers.py
@@ -11,10 +11,50 @@ from pyrogram.errors import StickersetInvalid, YouBlockedUser
 from pyrogram.raw.functions.messages import GetStickerSet
 from pyrogram.raw.types import InputStickerSetShortName
 
-from helpers.pyrohelp import get_arg, convert_to_image
+from helpers.pyrohelp import get_arg
 
 
-
+async def convert_to_image(message, client) -> Union[None, str]:
+    """
+    Convert Most Media Formats To Raw Image
+    """
+    if not message:
+        return None
+    if not message.reply_to_message:
+        return None
+    final_path = None
+    if not (
+        message.reply_to_message.video
+        or message.reply_to_message.photo
+        or message.reply_to_message.sticker
+        or message.reply_to_message.media
+        or message.reply_to_message.animation
+        or message.reply_to_message.audio
+    ):
+        return None
+    if message.reply_to_message.photo:
+        final_path = await message.reply_to_message.download()
+    elif message.reply_to_message.sticker:
+        if message.reply_to_message.sticker.mime_type == "image/webp":
+            final_path = "webp_to_png_s_proton.png"
+            path_s = await message.reply_to_message.download()
+            im = Image.open(path_s)
+            im.save(final_path, "PNG")
+        else:
+            path_s = await client.download_media(message.reply_to_message)
+            final_path = "lottie_proton.png"
+            cmd = (
+                f"lottie_convert.py --frame 0 -if lottie -of png {path_s} {final_path}"
+            )
+            await run_cmd(cmd)
+    elif message.reply_to_message.audio:
+        thumb = message.reply_to_message.audio.thumbs[0].file_id
+        final_path = await client.download_media(thumb)
+    elif message.reply_to_message.video or message.reply_to_message.animation:
+        final_path = "fetched_thumb.png"
+        vid_path = await client.download_media(message.reply_to_message)
+        await run_cmd(f"ffmpeg -i {vid_path} -filter:v scale=500:500 -an {final_path}")
+    return final_path
 
 
 @Client.on_message(filters.command("packinfo", ["."]) & filters.me)
