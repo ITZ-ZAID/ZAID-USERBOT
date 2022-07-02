5665528ab07a457be225dc665da283b7c412f7c2
Zaid <itzzaid@pm.me>
2022-07-02 20:27:39 +0530

    Update nekobin.py

--- handlers/nekobin.py
+++ handlers/nekobin.py
@@ -1,30 +1,32 @@
+from handlers.help import add_command_help
 import asyncio
-
+import requests
 import aiohttp
 from pyrogram import filters, Client
 from pyrogram.types import Message
-from handlers.help import add_command_help
+
+
 
 
 @Client.on_message(
-    filters.command(["neko", "nekobin", "bin", "paste"], ".") & filters.me
+    filters.command(["pasty"], ["."]) & filters.me
 )
-async def paste(_, message: Message):
+async def paste(app: Client, message: Message):
     text = message.reply_to_message.text
     try:
-        async with aiohttp.ClientSession() as session:
-            async with session.post(
-                "https://nekobin.com/api/documents", json={"content": text}, timeout=3
-            ) as response:
-                key = (await response.json())["result"]["key"]
+        params = {"content": text}
+        headers = {'content-type' : 'application/json'}
+        url = "https://pasty.lus.pm/api/v2/pastes/"
+        paste = requests.post(url, json=params, headers=headers)
+        key = paste.json()["id"]
     except Exception:
-        await message.edit_text("`Pasting failed`")
+        await message.edit_text("`API is down try again later`")
         await asyncio.sleep(2)
         await message.delete()
         return
     else:
-        url = f"https://nekobin.com/{key}"
-        reply_text = f"Nekofied to **Nekobin** : {url}"
+        url = f"https://pasty.lus.pm/{key}"
+        reply_text = f"**Pasted to: [Pasty]({url})\nRaw link: [Raw]({url}/raw)**"
         delete = (
             True
             if len(message.command) > 1
@@ -34,7 +36,7 @@ async def paste(_, message: Message):
         )
         if delete:
             await asyncio.gather(
-                UserBot.send_message(
+                app.send_message(
                     message.chat.id, reply_text, disable_web_page_preview=True
                 ),
                 message.reply_to_message.delete(),
@@ -46,6 +48,45 @@ async def paste(_, message: Message):
                 disable_web_page_preview=True,
             )
 
+@Client.on_message(
+    filters.command(["neko", "paste", "nekobin"], ["."]) & filters.me
+)
+async def neko(app: Client, message: Message):
+    text = message.reply_to_message.text
+    try:
+        params = {"content": text}
+        headers = {'content-type' : 'application/json'}
+        url = "https://nekobin.com/api/documents"
+        neko = requests.post(url, json=params, headers=headers)
+        key = neko.json()["result"]["key"]
+    except Exception:
+        await message.edit_text("`API is down try again later`")
+        await asyncio.sleep(2)
+        await message.delete()
+        return
+    else:
+        url = f"https://nekobin.com/{key}"
+        reply_text = f"**Pasted to: [Nekobin]({url})**"
+        delete = (
+            True
+            if len(message.command) > 1
+            and message.command[1] in ["d", "del"]
+            and message.reply_to_message.from_user.is_self
+            else False
+        )
+        if delete:
+            await asyncio.gather(
+                app.send_message(
+                    message.chat.id, reply_text, disable_web_page_preview=True
+                ),
+                message.reply_to_message.delete(),
+                message.delete(),
+            )
+        else:
+            await message.edit_text(
+                reply_text,
+                disable_web_page_preview=True,
+            )
 
 add_command_help(
     "paste",
