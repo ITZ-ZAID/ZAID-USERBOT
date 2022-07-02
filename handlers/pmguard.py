cc386f435a4b919499b1f2566070daf86a764e56
Zaid <itzzaid@pm.me>
2022-07-02 20:52:25 +0530

    Update pmguard.py

--- handlers/pmguard.py
+++ handlers/pmguard.py
@@ -1,6 +1,6 @@
 from pyrogram import filters, Client
 import asyncio
-
+from handlers.help import *
 from pyrogram.methods import messages
 from helpers.pyrohelper import get_arg, denied_users
 import helpers.mongo.pmpermitdb as Zaid
@@ -17,3 +17,17 @@ async def pmguard(client, message):
     if arg == "on":
         await Zaid.set_pm(True)
         await message.edit("**PM Guard Activated**")
+
+
+add_command_help(
+    "antipm",
+    [
+        [".pmguard [on or off]", " -> Activates or deactivates anti-pm."],
+        [".setpmmsg [message or default]", " -> Sets a custom anti-pm message."],
+        [".setblockmsg [message or default]", "-> Sets custom block message."],
+        [".setlimit [value]", " -> This one sets a max. message limit for unwanted PMs and when they go beyond it, bamm!."],
+        [".allow", " -> Allows a user to PM you."],
+        [".deny", " -> Denies a user to PM you."],
+    ],
+)
+
