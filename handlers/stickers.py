69e96048921c22899d076c77a1de726fbc8f7e94
Zaid <itzzaid@pm.me>
2022-07-02 19:16:57 +0530

    Update stickers.py

--- handlers/stickers.py
+++ handlers/stickers.py
@@ -220,8 +220,8 @@ def resize_photo(photo: str) -> io.BytesIO:
     return resized_photo
 
 
-async def get_response(app: Client, message):
-    return [x async for x in app.iter_history("Stickers", limit=1)][0].text
+async def get_response(message):
+    return [x async for x in Client.iter_history("Stickers", limit=1)][0].text
 
 
 KANGING_STR = (
