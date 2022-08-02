ae5c42455f828eb6bb072bd3bd44a2f7e7888e65
Zaid <itzzaid@pm.me>
2022-08-02 11:53:31 +0530

    Update gmutedb.py

--- helpers/mongo/gmutedb.py
+++ helpers/mongo/gmutedb.py
@@ -1,24 +1,16 @@
 from helpers.mongo import cli
 
-collection = cli["Zaid"]["gmute"]
+gmuteh = cli["GMUTE"]
 
 
-async def gmute_user(chat):
-    doc = {"_id": "Gmute", "users": [chat]}
-    r = await collection.find_one({"_id": "Gmute"})
-    if r:
-        await collection.update_one({"_id": "Gmute"}, {"$push": {"users": chat}})
-    else:
-        await collection.insert_one(doc)
+async def is_gmuted(sender_id):
+    kk = await gmuteh.find_one({"sender_id": sender_id})
+    return bool(kk)
 
 
-async def get_gmuted_users():
-    results = await collection.find_one({"_id": "Gmute"})
-    if results:
-        return results["users"]
-    else:
-        return []
+async def gmute(sender_id, reason="#GMuted"):
+    await gmuteh.insert_one({"sender_id": sender_id, "reason": reason})
 
 
-async def ungmute_user(chat):
-    await collection.update_one({"_id": "Gmute"}, {"$pull": {"users": chat}})
+async def ungmute(sender_id):
+    await gmuteh.delete_one({"sender_id": sender_id})
