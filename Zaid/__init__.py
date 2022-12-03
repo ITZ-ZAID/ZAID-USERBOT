1e811be269afc5c8445dcf984df63e3ea541be16
Zaid <itsunknown122@gmail.com>
2022-12-03 12:50:41 +0530

    Add files via upload

--- Zaid/__init__.py
+++ Zaid/__init__.py
@@ -1,1 +1,89 @@
+from pyrogram import Client
+from config import API_ID, API_HASH, SUDO_USERS, OWNER_ID, BOT_TOKEN, STRING_SESSION1, STRING_SESSION2, STRING_SESSION3, STRING_SESSION4, STRING_SESSION5, STRING_SESSION6, STRING_SESSION7, STRING_SESSION8, STRING_SESSION9, STRING_SESSION10
+from datetime import datetime
+import time
+from aiohttp import ClientSession
 
+StartTime = time.time()
+START_TIME = datetime.now()
+CMD_HELP = {}
+SUDO_USER = SUDO_USERS
+clients = []
+ids = []
+
+SUDO_USERS.append(OWNER_ID)
+aiosession = ClientSession()
+
+if API_ID:
+   API_ID = API_ID
+else:
+   print("WARNING: API ID NOT FOUND USING ZAID API ⚡")
+   API_ID = "6435225"
+
+if API_HASH:
+   API_HASH = API_HASH
+else:
+   print("WARNING: API HASH NOT FOUND USING ZAID API ⚡")   
+   API_HASH = "4e984ea35f854762dcde906dce426c2d"
+
+if not BOT_TOKEN:
+   print("WARNING: BOT TOKEN NOT FOUND PLZ ADD ⚡")   
+
+app = Client(
+    name="app",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN,
+    plugins=dict(root="Zaid/modules/bot"),
+    in_memory=True,
+)
+
+if STRING_SESSION1:
+   print("Client1: Found.. Starting..📳")
+   client1 = Client(name="one", api_id=API_ID, api_hash=API_HASH, session_string=STRING_SESSION1, plugins=dict(root="Zaid/modules"))
+   clients.append(client1)
+
+if STRING_SESSION2:
+   print("Client2: Found.. Starting.. 📳")
+   client2 = Client(name="two", api_id=API_ID, api_hash=API_HASH, session_string=STRING_SESSION2, plugins=dict(root="Zaid/modules"))
+   clients.append(client2)
+
+if STRING_SESSION3:
+   print("Client3: Found.. Starting.. 📳")
+   client3 = Client(name="three", api_id=API_ID, api_hash=API_HASH, session_string=STRING_SESSION3, plugins=dict(root="Zaid/modules"))
+   clients.append(client3)
+
+if STRING_SESSION4:
+   print("Client4: Found.. Starting.. 📳")
+   client4 = Client(name="four", api_id=API_ID, api_hash=API_HASH, session_string=STRING_SESSION4, plugins=dict(root="Zaid/modules"))
+   clients.append(client4)
+
+if STRING_SESSION5:
+   print("Client5: Found.. Starting.. 📳")
+   client5 = Client(name="five", api_id=API_ID, api_hash=API_HASH, session_string=STRING_SESSION5, plugins=dict(root="Zaid/modules"))
+   clients.append(client5)
+
+if STRING_SESSION6:
+   print("Client6: Found.. Starting.. 📳")
+   client6 = Client(name="six", api_id=API_ID, api_hash=API_HASH, session_string=STRING_SESSION6, plugins=dict(root="Zaid/modules"))
+   clients.append(client6)
+
+if STRING_SESSION7:
+   print("Client7: Found.. Starting.. 📳")
+   client7 = Client(name="seven", api_id=API_ID, api_hash=API_HASH, session_string=STRING_SESSION7, plugins=dict(root="Zaid/modules"))
+   clients.append(client7)
+
+if STRING_SESSION8:
+   print("Client8: Found.. Starting.. 📳")
+   client8 = Client(name="eight", api_id=API_ID, api_hash=API_HASH, session_string=STRING_SESSION8, plugins=dict(root="Zaid/modules"))
+   clients.append(client8)
+
+if STRING_SESSION9:
+   print("Client9: Found.. Starting.. 📳")
+   client9 = Client(name="nine", api_id=API_ID, api_hash=API_HASH, session_string=STRING_SESSION9, plugins=dict(root="Zaid/modules"))
+   clients.append(client9)
+
+if STRING_SESSION10:
+   print("Client10: Found.. Starting.. 📳")
+   client10 = Client(name="ten", api_id=API_ID, api_hash=API_HASH, session_string=STRING_SESSION10, plugins=dict(root="Zaid/modules")) 
+   clients.append(client10)
---
+++ Zaid/__main__.py
@@ -0,0 +1,26 @@
+import asyncio
+import importlib
+from pyrogram import Client, idle
+from Zaid.helper import join
+from Zaid.modules import ALL_MODULES
+from Zaid import clients, app, ids
+
+async def start_bot():
+    await app.start()
+    print("LOG: Founded Bot token Booting..")
+    for all_module in ALL_MODULES:
+        importlib.import_module("Zaid.modules" + all_module)
+        print("Successfully Imported Modules 💥")
+    for cli in clients:
+        try:
+            await cli.start()
+            ex = await cli.get_me()
+            await join(cli)
+            print(f"Started {ex.first_name} 🔥")
+            ids.append(ex.id)
+        except Exception as e:
+            print(f"{e}")
+    await idle()
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(start_bot())
