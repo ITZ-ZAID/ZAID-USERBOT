733b2a7f0f34da239ea75254fdd03d86e094ddd1
Bssamjh <112725901+Bssamjh@users.noreply.github.com>
2022-12-08 03:33:44 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,19 +3,19 @@ from os import getenv
 API_ID = int(getenv("API_ID", "6435225")) #optional
 API_HASH = getenv("API_HASH", "") #optional
 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
-OWNER_ID = int(getenv("OWNER_ID", ""))
-MONGO_URL = getenv("MONGO_URL")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5572426405").split()))
+OWNER_ID = int(getenv("OWNER_ID", "5572426405")) #ur id
+MONGO_URL = getenv("MONGO_URL") # an database
+BOT_TOKEN = getenv("BOT_TOKEN", "5780716702:AAGJ4GbwUNSnixolS4Avyn_HdPWlvaCTSvE")
 ALIVE_PIC = getenv("ALIVE_PIC")
-ALIVE_TEXT = getenv("ALIVE_TEXT")
-PM_LOGGER = getenv("PM_LOGGER")
-LOG_GROUP = getenv("LOG_GROUP")
+ALIVE_TEXT = getenv("ALIVE_TEXT") #optional
+PM_LOGGER = getenv("PM_LOGGER") # i'd if uh want
+LOG_GROUP = getenv("LOG_GROUP") #id if uh want to enable tagalert
 GIT_TOKEN = getenv("GIT_TOKEN") #personal access token
 REPO_URL = getenv("REPO_URL", "https://github.com/ITZ-ZAID/ZAID-USERBOT")
 BRANCH = getenv("BRANCH", "master") #don't change
  
-STRING_SESSION1 = getenv("STRING_SESSION1", "")
+STRING_SESSION1 = getenv("STRING_SESSION1", "BAAY9pRcRjZxRyW8hjKbGerqsbxUPl2NhfxtxZPgEZvRW0oXVEowvjpYqATbkr91iRyO8GDoXyFUHFPPcz-L3zPs5RRTn4fT_TF8CL8NV0tIudftbHopZXd5suW7IthX3fDSGy-Lxo-NgBI5sb8HTREKSscNPIUfhj81kJBWyOuEoA_JvU-3Q7QSW8p8mpBo6TfV-TU0Z3d4x1rGLKJDZDXEhUlrFIYIFZI1kzLjI9_PmsyfGIDkoJFGd2SYqLOL4cKg_NsD-Vsx1vWjovjwDxLCmitO45x_ww0kmv1BfpiG6fKrqurKFyVnouCz2bVn1Wb1hf3Z_xxzgv6NLWeYjh8_AAAAAUwkeqUA")
 STRING_SESSION2 = getenv("STRING_SESSION2", "")
 STRING_SESSION3 = getenv("STRING_SESSION3", "")
 STRING_SESSION4 = getenv("STRING_SESSION4", "")
