0eddc5824f1e60aa5d750e981b58bfc66b90a7f5
SagarLopps174 <119650467+SagarLopps174@users.noreply.github.com>
2022-12-16 21:00:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,19 +3,19 @@ from os import getenv
 API_ID = int(getenv("API_ID", "6435225")) #optional
 API_HASH = getenv("API_HASH", "") #optional
 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
-OWNER_ID = int(getenv("OWNER_ID", ""))
-MONGO_URL = getenv("MONGO_URL")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5693572886").split()))
+OWNER_ID = int(getenv("OWNER_ID", "5693572886"))
+MONGO_URL = getenv("MONGO_URL", "mongodb+srv://TgspamBot:TgspamBot@cluster0.36prwxa.mongodb.net/?retryWrites=true&w=majority")
+BOT_TOKEN = getenv("BOT_TOKEN", "5844187619:AAEMCJodo-wsUdMPae-i5hFXQndBJCT6W80")
 ALIVE_PIC = getenv("ALIVE_PIC")
-ALIVE_TEXT = getenv("ALIVE_TEXT")
-PM_LOGGER = getenv("PM_LOGGER")
-LOG_GROUP = getenv("LOG_GROUP")
+ALIVE_TEXT = getenv("ALIVE_TEXT") #optional
+PM_LOGGER = getenv("PM_LOGGER") # i'd if uh want
+LOG_GROUP = getenv("LOG_GROUP") # i'd if uh want to enable tagalert
 GIT_TOKEN = getenv("GIT_TOKEN") #personal access token
 REPO_URL = getenv("REPO_URL", "https://github.com/ITZ-ZAID/ZAID-USERBOT")
 BRANCH = getenv("BRANCH", "master") #don't change
  
-STRING_SESSION1 = getenv("STRING_SESSION1", "")
+STRING_SESSION1 = getenv("STRING_SESSION1", "AQBiMZkApcqUJ-u4dR1HJu__dFA2AyS6q6k5t8s_o7CjQJYsvko28Khec-c89lY-ZX4xeQPeki1YUFv7-lLYNxSh8SjUn3kSBHk2z4xjgjVw9FMTsszjlVvvE9BAjz95OPiS3Sieh5tBoFtK-pZto-AwRPEe3mjRgeMMTEJVA03TmSGuZ6CBPtxe-3ZhhJQcQVgBo7RblerddFhGmJHXok0NtLW8HXQIz6KkAQ9d0U0XF3qEsykB8L73inDYxG-Oq9y9Te4m6T5XAwih2w56adeGejnKL-8Ff0qPqATGd1RVQZXFQlsItmuQEHHY9zPzbMAzjVfFDVAGndhNRx6fX0FgNaMNEQAAAAFTXQcWAA")
 STRING_SESSION2 = getenv("STRING_SESSION2", "")
 STRING_SESSION3 = getenv("STRING_SESSION3", "")
 STRING_SESSION4 = getenv("STRING_SESSION4", "")
