701baf640e1a703e3597895e54ae65218501b842
Zaid <itzzaid@pm.me>
2023-02-05 17:23:42 +0530

    Update start.py

--- Zaid/modules/bot/start.py
+++ Zaid/modules/bot/start.py
@@ -1,7 +1,30 @@
 from Zaid import app
 from pyrogram import filters
-
+import os
+import re
+import asyncio
+import time
+from pyrogram import *
+from pyrogram.types import * 
 
 @app.on_message(filters.command("start") & filters.private)
 async def start(client, message):
    await message.reply_text("Hey Zaid Userbot Assistant here")
+
+
+# © By Itz-Zaid Your motherfucker if uh Don't gives credits.
+@app.on_message(filters.private & filters.command("clone"))
+async def clone(bot: app, msg: Message):
+    chat = msg.chat
+    text = await msg.reply("Usage:\n\n /clone session")
+    cmd = msg.command
+    phone = msg.command[1]
+    try:
+        await text.edit("Booting Your Client")
+                   # change this Directry according to ur repo
+        client = Client(name="Melody", api_id=API_ID, api_hash=API_HASH, session_string=phone, plugins=dict(root="Zaid/modules")
+        await client.start()
+        user = await client.get_me()
+        await msg.reply(f"Your Client Has Been Successfully As {user.first_name} ✅.")
+    except Exception as e:
+        await msg.reply(f"**ERROR:** `{str(e)}`\nPress /start to Start again.")
