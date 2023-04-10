b3c1c05cd7e5957211fe47a36cec73e8ce9a7bc6
☆*:.｡.ⲘⴑⲔⴑⲚⲆ.｡.:*☆ <fakemukund95@gmail.com>
2023-04-10 12:23:03 +0530

    Update pmguard.py

--- Zaid/modules/private/pmguard.py
+++ Zaid/modules/private/pmguard.py
@@ -69,7 +69,7 @@ async def allow(client, message):
 @Client.on_message(filters.command(["deny", "dap", "disapprove", "dapp"], ["."]) & filters.me & filters.private)
 async def deny(client, message):
     chat_id = message.chat.id
-    await Zectdb.deny_user(chat_id)
+    await Zaid.deny_user(chat_id)
     await message.edit(f"**I have denied [you](tg://user?id={chat_id}) to PM me.**")
 
 
