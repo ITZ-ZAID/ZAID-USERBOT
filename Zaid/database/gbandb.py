bde73248bd324f8f26f4a66fcd5d96e523250885
☆*:.｡.ⲘⴑⲔⴑⲚⲆ.｡.:*☆ <fakemukund95@gmail.com>
2023-06-01 23:39:11 +0530

    Update gbandb.py

--- Zaid/database/gbandb.py
+++ Zaid/database/gbandb.py
@@ -8,8 +8,12 @@ gbansdb = db.gban
 async def gban_list() -> list:
     users = gbansdb.find({"user_id": {"$gt": 0}})
     users = await users.to_list(length=100000)
-    return len(users)
+    return users
 
+async def gban_count() -> int:
+    users = gbansdb.find({"user_id": {"$gt": 0}})
+    users = await users.to_list(length=100000)
+    return len(users)
 
 async def gban_info(user_id: int) -> bool:
     user = await gbansdb.find_one({"user_id": user_id})
@@ -17,13 +21,11 @@ async def gban_info(user_id: int) -> bool:
         return False
     return True
 
-
 async def gban_user(user_id: int):
     is_gbanned = await gban_info(user_id)
     if is_gbanned:
         return
     return await gbansdb.insert_one({"user_id": user_id})
-
 
 async def ungban_user(user_id: int):
     is_gbanned = await gban_info(user_id)
