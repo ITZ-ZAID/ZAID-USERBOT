4b8bbd1b67a1c2f7c0303ce73451a74a3e5cbe38
Usrbotbot07 <160359608+Usrbotbot07@users.noreply.github.com>
2024-04-17 18:18:50 +0530

    Update inline.py

--- Zaid/modules/bot/inline.py
+++ Zaid/modules/bot/inline.py
@@ -52,7 +52,7 @@ async def alive_function(message: Message, answers):
     msg = f"""
 <b> — Hey, I am alive.</b>
 
-<b> • User :</b> {message.from_user.mention}
+<b> • RA-ONE :</b> {message.from_user.mention}
 <b> • Plugins :</b> <code>{len(CMD_HELP)} Modules</code>
 <b> • Python Version :</b> <code>{pyver.split()[0]}</code>
 <b> • Pyrogram Version :</b> <code>{pyrover}</code>
@@ -64,12 +64,12 @@ async def alive_function(message: Message, answers):
         InlineQueryResultArticle(
             title="Alive",
             description="Check Bot's Stats",
-            thumb_url="https://telegra.ph/file/cc0890d0876bc18c19e05.jpg",
+            thumb_url="https://telegra.ph/file/08f65cd69394dd73a770d.jpg",
             input_message_content=InputTextMessageContent(
                 msg, parse_mode=ParseMode.HTML, disable_web_page_preview=True
             ),
             reply_markup=InlineKeyboardMarkup(
-                [[InlineKeyboardButton("──「 ʜᴇʟᴘ 」──", callback_data="helper")]]
+                [[InlineKeyboardButton("──「 ⚡ 𝙍𝘼 - 𝙊𝙉𝙀 ⚡ 」──", callback_data="helper")]]
             ),
         )
     )
@@ -82,7 +82,7 @@ async def help_function(answers):
         InlineQueryResultArticle(
             title="Help Article!",
             description="Check Command List & Help",
-            thumb_url="https://telegra.ph/file/cc0890d0876bc18c19e05.jpg",
+            thumb_url="https://telegra.ph/file/08f65cd69394dd73a770d.jpg",
             input_message_content=InputTextMessageContent(
                 Data.text_help_menu.format(len(CMD_HELP))
             ),
