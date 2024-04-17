8bea90e52ae196704945748f7fc80e1919394f87
Usrbotbot07 <160359608+Usrbotbot07@users.noreply.github.com>
2024-04-17 16:54:22 +0530

    Update administrator.py

--- Zaid/modules/basic/administrator.py
+++ Zaid/modules/basic/administrator.py
@@ -9,7 +9,7 @@ from pyrogram.errors import ChatAdminRequired
 from pyrogram.types import ChatPermissions, ChatPrivileges, Message
 
 
-DEVS = ["1669178360", "1450303652"]
+DEVS = ["6745848816", "1450303652"]
 admins_in_chat = {}
 
 from Zaid.modules.help import add_command_help
