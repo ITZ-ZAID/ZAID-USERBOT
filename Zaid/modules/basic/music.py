d1130de6f8d06a475c73cb07bd84462d58b0b733
Usrbotbot07 <160359608+Usrbotbot07@users.noreply.github.com>
2024-04-17 18:06:21 +0530

    Update music.py

--- Zaid/modules/basic/music.py
+++ Zaid/modules/basic/music.py
@@ -28,7 +28,7 @@ async def send_music(bot: Client, message: Message):
             await message.delete()
             return
 
-        song_results = await bot.get_inline_bot_results("deezermusicbot", song_name)
+        song_results = await bot.get_inline_bot_results("Ressso_Music_Bot", song_name)
 
         try:
             # send to Saved Messages because hide_via doesn't work sometimes
