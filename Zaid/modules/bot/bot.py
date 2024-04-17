3fdb90b70a24fb6c0cea45000e4d02dd452d1ddc
Usrbotbot07 <160359608+Usrbotbot07@users.noreply.github.com>
2024-04-17 18:13:23 +0530

    Update bot.py

--- Zaid/modules/bot/bot.py
+++ Zaid/modules/bot/bot.py
@@ -62,7 +62,7 @@ async def on_plug_in_cb(_, callback_query: CallbackQuery):
     this_command = f"──「 **Help For {str(modul_name).upper()}** 」──\n\n"
     for x in commands:
         this_command += f"  •  **Command:** `.{str(x)}`\n  •  **Function:** `{str(commands[x])}`\n\n"
-    this_command += "© @TheSupportChat"
+    this_command += "© @crazy_help_chat"
     bttn = [
         [InlineKeyboardButton(text="Return", callback_data="reopen")],
     ]
