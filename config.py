304371e6eb6f4f8f6f085dcd32e1c116f712645b
𓆩◕🇭𝐀𝐑𝐑𝐘◕𓆪 =‌𐏓 𝄗⃝🇮🇳™ <ronnie271713@gmail.com>
2024-08-18 18:00:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,9 +11,9 @@ API_HASH = getenv("API_HASH", "cb070192e4e4f01b7d314a80876a4f31") #optional
 
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
 OWNER_ID = int(getenv("OWNER_ID", "")
-MONGO_URL = getenv("MONGO_URL", "")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-ALIVE_PIC = getenv("ALIVE_PIC", 'https://telegra.ph/file/3c52a01057865f7511168.jpg')
+MONGO_URL = int(getenv("MONGO_URL", "")
+BOT_TOKEN = int(getenv("BOT_TOKEN", "")
+ALIVE_PIC = int(getenv("ALIVE_PIC", 'https://telegra.ph/file/3c52a01057865f7511168.jpg')
 ALIVE_TEXT = getenv("ALIVE_TEXT")
 PM_LOGGER = getenv("PM_LOGGER")
 LOG_GROUP = getenv("LOG_GROUP", "")
