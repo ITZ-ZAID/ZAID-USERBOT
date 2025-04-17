091974c6d71783f1d87f5ad7e24e868be923dc6a
Zaid <itzzaid@pm.me>
2025-04-18 00:02:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ if os.path.exists("local.env"):
 
 
 API_ID = int(getenv("API_ID", "6435225")) #optional
-API_HASH = getenv("API_HASH", "") #optional
+API_HASH = getenv("API_HASH", "4e984ea35f854762dcde906dce426c2d") #optional
 
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
 OWNER_ID = int(getenv("OWNER_ID"))
